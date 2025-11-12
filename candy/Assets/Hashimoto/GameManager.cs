36f1f21ebaa4d81670b8cf60d046d8dcd1873d38
Teranishi_Souki <h1202516003@hiratagakuen.onmicrosoft.com>
2025-11-12 11:22:31 +0900

    no message

--- candy/Assets/Hashimoto/GameManager.cs
+++ candy/Assets/Hashimoto/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
+
         if (instance == null)
         {
             instance = this;
