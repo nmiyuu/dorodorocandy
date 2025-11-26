85cc969b8c86b8aecb5c4738c28cc603684b653d
hashimotokaisyo <h1202515005@hiratagakuen.onmicrosoft.com>
2025-11-26 15:52:08 +0900

    ギミック2完了

--- candy/Assets/Hashimoto/GameManager.cs
+++ candy/Assets/Hashimoto/GameManager.cs
@@ -1,86 +1,26 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    public static GameManager Instance;
 
-    public GameObject playerPrefab;          // プレイヤーのPrefab
-    public Vector3 playerStartPos = new Vector3(0, -1, 0); // プレイヤー初期位置
-    public int count = 0;
+    // アイテム取得状態（キー: アイテムID）
+    public HashSet<string> obtainedItems = new HashSet<string>();
 
+    // ブロック破壊状態（キー: ブロックID）
+    public HashSet<string> destroyedBlocks = new HashSet<string>();
 
     void Awake()
     {
-        if (instance == null)
+        if (Instance == null)
         {
-            instance = this;
-            DontDestroyOnLoad(gameObject); // 常駐
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            StartCoroutine(ResetGame());
-        }
-
-        count++;
-    }
-
-    IEnumerator ResetGame()
-    {
-        Debug.Log("ゲームリセット開始...");
-
-        // ① 古いプレイヤーを削除
-        GameObject oldPlayer = GameObject.FindGameObjectWithTag("Player");
-        if (oldPlayer != null)
-        {
-            Destroy(oldPlayer);
-            yield return null; // 1フレーム待つ
-        }
-
-        // ② Scene1, Scene2 をアンロード
-        int sceneCount = SceneManager.sceneCount;
-        List<string> scenesToReload = new List<string>();
-
-        for (int i = 0; i < sceneCount; i++)
-        {
-            Scene scene = SceneManager.GetSceneAt(i);
-            if (scene.name != "PersistentScene" && scene.isLoaded)
-            {
-                scenesToReload.Add(scene.name);
-            }
-        }
-
-        foreach (string sceneName in scenesToReload)
-        {
-            AsyncOperation unload = SceneManager.UnloadSceneAsync(sceneName);
-            while (!unload.isDone)
-                yield return null;
-        }
-
-        // ③ Scene1, Scene2 を再ロード
-        foreach (string sceneName in scenesToReload)
-        {
-            AsyncOperation load = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-            while (!load.isDone)
-                yield return null;
-        }
-
-        // ④ プレイヤーを生成
-        if (playerPrefab != null)
-        {
-            Instantiate(playerPrefab, playerStartPos, Quaternion.identity);
-        }
-
-        Debug.Log("ゲームリセット完了。オブジェクトもプレイヤーも初期化されました。");
     }
 }
