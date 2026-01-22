78f2aff9987459bbeae604510f538a6d5782b4ad
niiyamiyuu <h1202515002@hiratagakuen.onmicrosoft.com>
2026-01-22 10:16:23 +0900

    Revert "コメント"
    This reverts commit 2b874390f26aac4f7b4d7a13830fdaf0400d9285.

--- candy/Assets/Niiya/navimanager.cs
+++ candy/Assets/Niiya/navimanager.cs
@@ -11,7 +11,7 @@ public class NaviManager : MonoBehaviour
 
 
     [Header("Settings")]
-    public float typeSpeed = 0.05f;// 文字が表示されるスピード
+    public float typeSpeed = 0.05f;
 
     [Header("Messages")]
     public string[] naviMessages = {
@@ -29,8 +29,8 @@ public class NaviManager : MonoBehaviour
     };
 
     int index = 0;
-    bool isTyping = false;// 現在タイピング中かどうか
-    bool canPressEnter = false;// Enterキーを押せる状態か
+    bool isTyping = false;
+    bool canPressEnter = false;
 
     void Start()
     {
@@ -55,16 +55,13 @@ public class NaviManager : MonoBehaviour
 
     void Update()
     {
-        // Enterを押せない状態なら何もしない
         if (!canPressEnter) return;
 
-        // Enterキーが押されたら次のセリフへ
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             index++;
             canPressEnter = false;
 
-            // まだセリフが残っている場合
             if (index < naviMessages.Length)
             {
                 StartCoroutine(TypeNaviText(naviMessages[index]));
@@ -72,7 +69,7 @@ public class NaviManager : MonoBehaviour
             }
             else
             {
-                HideAll();// 全部のセリフを表示し終わったらUIを消す
+                HideAll();
                 PlayerPrefs.SetInt("NaviShown", 1);
 
                 //全会話終了：フラグを false にしてプレイヤーを動けるようにする
